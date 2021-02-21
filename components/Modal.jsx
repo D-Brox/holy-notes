384cc9814806517f40b3615f6c3dcdda95754e84
D Brox <65723952+D-Brox@users.noreply.github.com>
2021-02-21 11:50:11 -0300

    Update Modal.jsx
    Add delete button to UI

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,7 +1,7 @@
 const { getModule, getModuleByDisplayName, React } = require('powercord/webpack');
 const { Modal } = require('powercord/components/modal');
-const { FormTitle, Text } = require('powercord/components');
-const { close: closeModal } = require('powercord/modal');
+const { FormTitle, Text, Divider, Tooltip, Icon, Button } = require('powercord/components');
+const { close: closeModal, open: openModal } = require('powercord/modal');
 const { getCurrentUser, getUser } = getModule([ 'getCurrentUser' ], false);
 
 const NotesHandler = new (require('../NotesHandler'))();
@@ -26,51 +26,127 @@ class noteDisplay extends React.PureComponent {
     render() {
         const noteArray = [];
         let NoteMessage
+        let title
+        let size
+        let buttons = <></>
         if(this.props.all){
             const notes = NotesHandler.getNotes();
-
             for(let i = 0; i < Object.keys(notes).length; i++) {
                 let note = notes[Object.keys(notes)[i]]
-
+                let divider
 			    const user = UserStore.getUser(note['User_ID']);
-			    NoteMessage = <ChannelMessage
-			    message={new Message({ author: user, content: note['Content'], attachments: note['Attachment'] || [], embeds: note['Embeds'] || [], id: note['Message_ID']})}
-			    channel={channel}/>
+			    NoteMessage = <div className='NoteMessages'>
+                    <ChannelMessage
+			            message={
+                            new Message({
+                                author: user,
+                                content: note['Content'],
+                                attachments: note['Attachment'] || [],
+                                embeds: note['Embeds'] || [],
+                                id: note['Message_ID']
+                        })}
+			            channel={channel}                        
+                        //onDoubleClick={openModal(() => React.createElement(noteDisplay,{note,all:false,del:false}))} // This didn't work.also tried ondblClick
+                    />
+                    <div className='deleteNote'
+
+                        ><Tooltip
+                            position='left'
+                            text='Cancel Quoting Message'
+                            
+                        ><Icon name='Trash' color='var(--interactive-normal)' onClick={() => {
+                            openModal(() => React.createElement(noteDisplay,{note,all:false,del:true}))
+                            this.forceUpdate()
+                        }}/>
+                        </Tooltip>
+                    </div>
+                </div>
 			    noteArray.push(NoteMessage)
-			    noteArray.push(<br/>)
+                
+			    divider = (i===(Object.keys(notes).length-1))?<></>:<Divider/>
+                noteArray.push(divider)
+                noteArray.push(<br/>)
             }
-            return(	
-            <Modal className='Notebook' size={Modal.Sizes.LARGE}>
-                <Modal.Header>
-                <FormTitle tag='h3'>Notebook</FormTitle>
-                <Modal.CloseButton onClick={closeModal}/>
-                </Modal.Header>
-
-                <Modal.Content>
-						    {noteArray}
-
-                </Modal.Content>
-            </Modal>
-            )
+            title = 'Notebook'
+            size = Modal.Sizes.LARGE
         } else {
             const note = this.props.note
 		    const user = UserStore.getUser(note['User_ID']);
-		    NoteMessage = <ChannelMessage
-		    message={new Message({ author: user, content: note['Content'], attachments: note['Attachment'] || [], embeds: note['Embeds'] || [], id: note['Message_ID']})}
-		    channel={channel}/>
+		    NoteMessage = <div className='NoteMessages'>
+                    <ChannelMessage
+			            message={
+                            new Message({
+                                author: user,
+                                content: note['Content'],
+                                attachments: note['Attachment'] || [],
+                                embeds: note['Embeds'] || [],
+                                id: note['Message_ID']
+                        })}
+			            channel={channel}
+                    />
+                    <div className='deleteNote'
+
+                        ><Tooltip
+                            position='left'
+                            text='Cancel Quoting Message'
+                            
+                        ><Icon name='Trash' color='var(--interactive-normal)' onClick={() => {
+                            openModal(() => React.createElement(noteDisplay,{note,all:false,del:true}))
+                            this.forceUpdate()
+                        }}/>
+                        </Tooltip>
+                    </div>
+                </div>
 		    noteArray.push(NoteMessage)
-            return(	
-                <Modal className='Notebook' size={Modal.Sizes.MEDIUM}>
-                    <Modal.Header>
-                    <FormTitle tag='h3'>{'Message '+note['Message_ID']}</FormTitle>
-                    <Modal.CloseButton onClick={closeModal}/>
-                    </Modal.Header>
-                    <Modal.Content>
-        						{noteArray}    
-                    </Modal.Content>
-                </Modal>
-            )
+            title = 'Message '+note['Message_ID']
+            size = Modal.Sizes.MEDIUM
+            if(this.props.del){
+                title = 'Delete Message '+note['Message_ID']+'?'
+                buttons= []
+                buttons.push(
+                    <Button
+                        className='delete button'
+                        color={Button.Colors.RED}
+                        onClick={ () => {
+                            closeModal()
+                            NotesHandler.deleteNote(note['Message_ID'])
+                        }}
+                    >Delete
+                    </Button>
+                )
+                buttons.push(
+                    <Button
+                        className='cancel button'
+                        style={{ marginRight: "10px" }} 
+                        color={Button.Colors.GRAY}
+                        onClick={closeModal}
+                    >Cancel
+                    </Button>)
+            }else {
+                buttons=<Button
+                    className='delete button'
+                    color={Button.Colors.GRAY}
+                    onClick={closeModal}
+                >Jump To Message
+                </Button>
+            }
+            console.log(buttons)
         }
+        try{return(	
+        <Modal className='Notebook' size={size}>
+            <Modal.Header>
+                <FormTitle tag='h3'>{title}</FormTitle>
+                <Modal.CloseButton onClick={closeModal}/>
+            </Modal.Header>
+
+            <Modal.Content>
+				    {noteArray}
+            </Modal.Content>
+            <Modal.Footer>
+                {buttons}
+            </Modal.Footer>
+        </Modal>
+        )}catch(err){console.log(err)}
     }
 }
 
